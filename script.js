894bc5e1114f323fd35ec220bd4dec52ce41794e
C Grogan <conorgrogan23@gmail.com>
2024-01-16 03:51:20 +0000

    Created function for prompt and edited event listener

---
+++ script.js
@@ -0,0 +1,131 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+// Function to prompt user for password options
+function getPasswordOptions() { 
+var passwordLength = prompt('Please enter the length of your password (between 8 and 128 characters');
+if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert ("Please enter a valid password length between 8 and 128 characters");
+    return;
+}
+
+var lowercase = confirm("Include lowercase characters?");
+var uppercase = confirm("Include uppercase characters?");
+var numbers = confirm("Include numeric characters?")
+var special = confirm("Include special characters?");
+
+if (!lowercase && !uppercase && !numbers && !special) {
+  alert('Please select at least one character type to generate your password')  
+  }
+}
+
+// Function for getting a random element from an array
+function getRandom() { 
+
+}
+
+// Function to generate password with user input
+function generatePassword() {
+
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+document.getElementById('generate').addEventListener('click', generatePassword);
